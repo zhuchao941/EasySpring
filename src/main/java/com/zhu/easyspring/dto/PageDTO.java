2b550167c6fdc974f403f8c50d9ccc9090933057
Zhu <339357762@qq.com>
2015-03-13 16:54:43 +0800

    排序用，暂时未用到

---
+++ src/main/java/com/zhu/easyspring/dto/PageDTO.java
@@ -0,0 +1,41 @@
+package com.zhu.easyspring.dto;
+
+public class PageDTO {
+	private int page;
+	private int rows;
+	private String sort;
+	private String order;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public String getSort() {
+		return sort;
+	}
+
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+
+	public String getOrder() {
+		return order;
+	}
+
+	public void setOrder(String order) {
+		this.order = order;
+	}
+
+}
