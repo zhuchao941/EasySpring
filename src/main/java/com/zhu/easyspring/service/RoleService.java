7d3e21b4a15c41035a1596b501ffc058a4c6437e
Zhu <339357762@qq.com>
2015-03-16 14:34:50 +0800

    添加方法getRoleList(Integer[] ids)

--- src/main/java/com/zhu/easyspring/service/RoleService.java
+++ src/main/java/com/zhu/easyspring/service/RoleService.java
@@ -1,7 +1,11 @@
 package com.zhu.easyspring.service;
 
+import java.util.List;
+
 import com.zhu.easyspring.entity.Role;
 
 public interface RoleService {
 	public Role getRoleById(Integer id);
+	
+	public List<Role> getRoleList(Integer[] roleIds);
 }
--- src/main/java/com/zhu/easyspring/service/impl/RoleServiceImpl.java
+++ src/main/java/com/zhu/easyspring/service/impl/RoleServiceImpl.java
@@ -1,11 +1,15 @@
 package com.zhu.easyspring.service.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.zhu.easyspring.dao.RoleMapper;
 import com.zhu.easyspring.entity.Role;
+import com.zhu.easyspring.entity.RoleExample;
 import com.zhu.easyspring.service.RoleService;
 
 @Service("roleService")
@@ -17,6 +21,14 @@ public class RoleServiceImpl implements RoleService {
 	@Override
 	public Role getRoleById(Integer id) {
 		return roleMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public List<Role> getRoleList(Integer[] roleIds) {
+		RoleExample example = new RoleExample();
+		example.createCriteria().andIdIn(Arrays.asList(roleIds));
+		example.setOrderByClause("id");
+		return roleMapper.selectByExample(example);
 	}
 
 }
