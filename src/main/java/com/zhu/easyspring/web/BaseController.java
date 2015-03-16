39fce5d3d59e1576fd739bc5e4afe3f798b8d9f4
zhu <zhu339357762@gmail.com>
2015-03-16 22:43:56 +0800

    添加全局的datagrid

--- src/main/java/com/zhu/easyspring/web/BaseController.java
+++ src/main/java/com/zhu/easyspring/web/BaseController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.zhu.easyspring.dto.UserPreferences;
+import com.zhu.easyspring.velocity.bean.Datagrid;
 
 @Controller
 public class BaseController {
@@ -14,6 +15,11 @@ public class BaseController {
 	@ModelAttribute("contextPath")
 	protected String contextPath(HttpServletRequest request) {
 		return request.getContextPath();
+	}
+	
+	@ModelAttribute("datagrid")
+	protected Datagrid datagrid(HttpServletRequest request) {
+		return new Datagrid();
 	}
 
 	protected UserPreferences getUserPreferences(HttpSession session) {
