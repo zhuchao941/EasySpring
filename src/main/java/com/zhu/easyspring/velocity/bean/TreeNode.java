1705a3352415b1dde0b8bf769bf91673b47520e4
Zhu <339357762@qq.com>
2015-03-18 11:16:59 +0800

    树节点定义

---
+++ src/main/java/com/zhu/easyspring/velocity/bean/TreeNode.java
@@ -0,0 +1,65 @@
+package com.zhu.easyspring.velocity.bean;
+
+import java.util.List;
+
+/**
+ * @author Zhu
+ * 
+ */
+public class TreeNode {
+	private String id;
+	private String text;
+	private String state;
+	private String iconCls;
+	private boolean checked;
+	private List<TreeNode> children;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getIconCls() {
+		return iconCls;
+	}
+
+	public void setIconCls(String iconCls) {
+		this.iconCls = iconCls;
+	}
+
+	public boolean isChecked() {
+		return checked;
+	}
+
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	public List<TreeNode> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<TreeNode> children) {
+		this.children = children;
+	}
+
+}
