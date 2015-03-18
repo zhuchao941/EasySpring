8721709c76c169ff8096878c7839a9658a7f5082
Zhu <339357762@qq.com>
2015-03-18 12:40:23 +0800

    数据库变更

--- src/main/resources/easySpring.sql
+++ src/main/resources/easySpring.sql
@@ -1,88 +1,75 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : test
-Source Server Version : 50156
+Source Server         : localhost_3306
+Source Server Version : 50151
 Source Host           : localhost:3306
 Source Database       : easy_spring
 
 Target Server Type    : MYSQL
-Target Server Version : 50156
+Target Server Version : 50151
 File Encoding         : 65001
 
-Date: 2015-03-16 00:12:14
+Date: 2015-03-13 17:18:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `resource`
+-- Table structure for `roles_permissions`
 -- ----------------------------
-DROP TABLE IF EXISTS `resource`;
-CREATE TABLE `resource` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`code`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`name`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`type`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`url`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-`icon`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-`parent`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+DROP TABLE IF EXISTS `roles_permissions`;
+CREATE TABLE `roles_permissions` (
+`id`  int(10) NOT NULL ,
+`role_name`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`permission`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-AUTO_INCREMENT=1
 
 ;
 
 -- ----------------------------
--- Table structure for `role`
+-- Records of roles_permissions
 -- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`code`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`name`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-`permission_ids`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-AUTO_INCREMENT=1
-
-;
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
 `username`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 `password`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
 `email`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
 `salt`  varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
 `status`  int(1) NOT NULL DEFAULT 1 ,
-`role_ids`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-PRIMARY KEY (`id`)
+PRIMARY KEY (`username`)
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-AUTO_INCREMENT=1
 
 ;
 
 -- ----------------------------
--- Auto increment value for `resource`
+-- Table structure for `user_roles`
 -- ----------------------------
-ALTER TABLE `resource` AUTO_INCREMENT=1;
+DROP TABLE IF EXISTS `user_roles`;
+CREATE TABLE `user_roles` (
+`id`  int(10) NOT NULL ,
+`username`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`role_name`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+
+;
 
 -- ----------------------------
--- Auto increment value for `role`
+-- Records of user_roles
 -- ----------------------------
-ALTER TABLE `role` AUTO_INCREMENT=1;
-
--- ----------------------------
--- Auto increment value for `user`
--- ----------------------------
-ALTER TABLE `user` AUTO_INCREMENT=1;
+BEGIN;
+COMMIT;
